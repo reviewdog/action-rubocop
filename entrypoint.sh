566303f55f011db91411b64c4be15730c64ea512
Mikhail Grachev <work@mgrachev.com>
2019-11-14 11:57:27 +0300

    Add support more than 50 annotations
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 rubocop ${INPUT_RUBOCOP_FLAGS} \
-  | reviewdog -f=rubocop -name="${INPUT_TOOL_NAME}" -reporter=github-pr-check -level="${INPUT_LEVEL}"
+  | /bin/reviewdog -f=rubocop -name="${INPUT_TOOL_NAME}" -reporter=github-pr-check -level="${INPUT_LEVEL}"
