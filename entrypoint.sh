86c424fde18cf19003ab5175c71216e026af3241
Alexey Matskevich <aleksei_mackevich_job@outlook.com>
2020-04-09 21:17:08 +0300

    Apply review comment
    # Use last version by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
+version() {
+  if [ -n "$1" ]; then
+    echo "-v $1"
+  fi
+}
 
 cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-gem install -N rubocop -v ${INPUT_RUBOCOP_VERSION} \
- rubocop-rails -v ${INPUT_RUBOCOP_RAILS_VERSION} \
-  rubocop-performance -v ${INPUT_RUBOCOP_PERFORMANCE_VERSION} \
-   rubocop-rspec -v ${INPUT_RUBOCOP_RSPEC_VERSION} \
-    rubocop-i18n -v ${INPUT_RUBOCOP_I18N_VERSION} \
-     rubocop-rake -v ${INPUT_RUBOCOP_RAKE_VERSION}
+gem install rubocop $(version $INPUT_RUBOCOP_VERSION)
+gem install rubocop-rails $(version $INPUT_RUBOCOP_RAILS_VERSION)
+gem install rubocop-performance $(version $INPUT_RUBOCOP_PERFORMANCE_VERSION)
+gem install rubocop-rspec $(version $INPUT_RUBOCOP_RSPEC_VERSION)
+gem install rubocop-i18n $(version $INPUT_RUBOCOP_I18N_VERSION)
+gem install rubocop-rake $(version $INPUT_RUBOCOP_RAKE_VERSION)
 
 rubocop ${INPUT_RUBOCOP_FLAGS} \
   | reviewdog -f=rubocop -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
