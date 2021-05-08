695537c0479de7c92ff81ede08be2d9a15c73caa
Jared Szechy <jared.szechy@gmail.com>
2021-05-08 12:45:03 -0400

    Add input to skip install

--- script.sh
+++ script.sh
@@ -15,70 +15,72 @@ echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/review
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
 echo '::endgroup::'
 
-echo '::group:: Installing rubocop with extensions ... https://github.com/rubocop/rubocop'
-# if 'gemfile' rubocop version selected
-if [ "${INPUT_RUBOCOP_VERSION}" = "gemfile" ]; then
-  # if Gemfile.lock is here
-  if [ -f 'Gemfile.lock' ]; then
-    # grep for rubocop version
-    RUBOCOP_GEMFILE_VERSION=$(ruby -ne 'print $& if /^\s{4}rubocop\s\(\K.*(?=\))/' Gemfile.lock)
-
-    # if rubocop version found, then pass it to the gem install
-    # left it empty otherwise, so no version will be passed
-    if [ -n "$RUBOCOP_GEMFILE_VERSION" ]; then
-      RUBOCOP_VERSION=$RUBOCOP_GEMFILE_VERSION
-      else
-        printf "Cannot get the rubocop's version from Gemfile.lock. The latest version will be installed."
-    fi
-    else
-      printf 'Gemfile.lock not found. The latest version will be installed.'
-  fi
-  else
-    # set desired rubocop version
-    RUBOCOP_VERSION=$INPUT_RUBOCOP_VERSION
-fi
-
-gem install -N rubocop --version "${RUBOCOP_VERSION}"
-
-# Traverse over list of rubocop extensions
-for extension in $INPUT_RUBOCOP_EXTENSIONS; do
-  # grep for name and version
-  INPUT_RUBOCOP_EXTENSION_NAME=$(echo "$extension" |awk 'BEGIN { FS = ":" } ; { print $1 }')
-  INPUT_RUBOCOP_EXTENSION_VERSION=$(echo "$extension" |awk 'BEGIN { FS = ":" } ; { print $2 }')
-
-  # if version is 'gemfile'
-  if [ "${INPUT_RUBOCOP_EXTENSION_VERSION}" = "gemfile" ]; then
+if [ "${INPUT_SKIP_INSTALL}" = "false" ]; then
+  echo '::group:: Installing rubocop with extensions ... https://github.com/rubocop/rubocop'
+  # if 'gemfile' rubocop version selected
+  if [ "${INPUT_RUBOCOP_VERSION}" = "gemfile" ]; then
     # if Gemfile.lock is here
     if [ -f 'Gemfile.lock' ]; then
-      # grep for rubocop extension version
-      RUBOCOP_EXTENSION_GEMFILE_VERSION=$(ruby -ne "print $& if /^\s{4}$INPUT_RUBOCOP_EXTENSION_NAME\s\(\K.*(?=\))/" Gemfile.lock)
+      # grep for rubocop version
+      RUBOCOP_GEMFILE_VERSION=$(ruby -ne 'print $& if /^\s{4}rubocop\s\(\K.*(?=\))/' Gemfile.lock)
 
-      # if rubocop extension version found, then pass it to the gem install
+      # if rubocop version found, then pass it to the gem install
       # left it empty otherwise, so no version will be passed
-      if [ -n "$RUBOCOP_EXTENSION_GEMFILE_VERSION" ]; then
-        RUBOCOP_EXTENSION_VERSION=$RUBOCOP_EXTENSION_GEMFILE_VERSION
+      if [ -n "$RUBOCOP_GEMFILE_VERSION" ]; then
+        RUBOCOP_VERSION=$RUBOCOP_GEMFILE_VERSION
         else
-          printf "Cannot get the rubocop extension version from Gemfile.lock. The latest version will be installed."
+          printf "Cannot get the rubocop's version from Gemfile.lock. The latest version will be installed."
       fi
       else
         printf 'Gemfile.lock not found. The latest version will be installed.'
     fi
-  else
-    # set desired rubocop extension version
-    RUBOCOP_EXTENSION_VERSION=$INPUT_RUBOCOP_EXTENSION_VERSION
+    else
+      # set desired rubocop version
+      RUBOCOP_VERSION=$INPUT_RUBOCOP_VERSION
   fi
 
-  # Handle extensions with no version qualifier
-  if [ -z "${RUBOCOP_EXTENSION_VERSION}" ]; then
-    unset RUBOCOP_EXTENSION_VERSION_FLAG
-  else
-    RUBOCOP_EXTENSION_VERSION_FLAG="--version ${RUBOCOP_EXTENSION_VERSION}"
-  fi
+  gem install -N rubocop --version "${RUBOCOP_VERSION}"
 
-  # shellcheck disable=SC2086
-  gem install -N "${INPUT_RUBOCOP_EXTENSION_NAME}" ${RUBOCOP_EXTENSION_VERSION_FLAG}
-done
-echo '::endgroup::'
+  # Traverse over list of rubocop extensions
+  for extension in $INPUT_RUBOCOP_EXTENSIONS; do
+    # grep for name and version
+    INPUT_RUBOCOP_EXTENSION_NAME=$(echo "$extension" |awk 'BEGIN { FS = ":" } ; { print $1 }')
+    INPUT_RUBOCOP_EXTENSION_VERSION=$(echo "$extension" |awk 'BEGIN { FS = ":" } ; { print $2 }')
+
+    # if version is 'gemfile'
+    if [ "${INPUT_RUBOCOP_EXTENSION_VERSION}" = "gemfile" ]; then
+      # if Gemfile.lock is here
+      if [ -f 'Gemfile.lock' ]; then
+        # grep for rubocop extension version
+        RUBOCOP_EXTENSION_GEMFILE_VERSION=$(ruby -ne "print $& if /^\s{4}$INPUT_RUBOCOP_EXTENSION_NAME\s\(\K.*(?=\))/" Gemfile.lock)
+
+        # if rubocop extension version found, then pass it to the gem install
+        # left it empty otherwise, so no version will be passed
+        if [ -n "$RUBOCOP_EXTENSION_GEMFILE_VERSION" ]; then
+          RUBOCOP_EXTENSION_VERSION=$RUBOCOP_EXTENSION_GEMFILE_VERSION
+          else
+            printf "Cannot get the rubocop extension version from Gemfile.lock. The latest version will be installed."
+        fi
+        else
+          printf 'Gemfile.lock not found. The latest version will be installed.'
+      fi
+    else
+      # set desired rubocop extension version
+      RUBOCOP_EXTENSION_VERSION=$INPUT_RUBOCOP_EXTENSION_VERSION
+    fi
+
+    # Handle extensions with no version qualifier
+    if [ -z "${RUBOCOP_EXTENSION_VERSION}" ]; then
+      unset RUBOCOP_EXTENSION_VERSION_FLAG
+    else
+      RUBOCOP_EXTENSION_VERSION_FLAG="--version ${RUBOCOP_EXTENSION_VERSION}"
+    fi
+
+    # shellcheck disable=SC2086
+    gem install -N "${INPUT_RUBOCOP_EXTENSION_NAME}" ${RUBOCOP_EXTENSION_VERSION_FLAG}
+  done
+  echo '::endgroup::'
+fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
