105b1a1cf51a02367e7e8349188472853a3fd446
ohbarye <over.rye@gmail.com>
2021-08-27 23:29:47 +0900

    Move rdjson formatter test files into test/

--- rdjson_formatter/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Expected to run from the root repository.
-set -eux
-CWD=$(pwd)
-rubocop ./testdata/*.rb --require ./rdjson_formatter/rdjson_formatter.rb --format RdjsonFormatter --cache false \
-  | jq . \
-  | sed -e "s!${CWD}/!!g" \
-  > rdjson_formatter/testdata/result.out
-diff -u rdjson_formatter/testdata/result.*
